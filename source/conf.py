af4ad3fb7362ba2119117af3b85ce5ec51866ad5
Huaqi Fang <578567190@qq.com>
2021-08-27 14:25:10 +0800

    ci: remove dups
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- source/conf.py
+++ source/conf.py
@@ -53,9 +53,6 @@ numfig = True
 
 suppress_warnings = ['autosectionlabel.*']
 
-# Show build timestamp
-html_last_updated_fmt = ""
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 source_suffix = ['.rst', '.md']
